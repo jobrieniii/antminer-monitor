aa0030b0f9ea53377a21bee8c93148fcf1a12458
jobrieniii <jobrieniii@yahoo.com>
2017-12-09 03:46:12 -0500

    add minimal dashboard

--- app/views/antminer.py
+++ app/views/antminer.py
@@ -156,6 +156,143 @@ def miners():
                            miner_errors=miner_errors,
                            )
 
+@app.route('/dashboard')
+def dashboard():
+    # Init variables
+    start = time.clock()
+    miners = Miner.query.all()
+    models = MinerModel.query.all()
+    active_miners = []
+    inactive_miners = []
+    workers = {}
+    miner_chips = {}
+    temperatures = {}
+    fans = {}
+    hash_rates = {}
+    hw_error_rates = {}
+    uptimes = {}
+    total_hash_rate_per_model = {"L3+": 0,
+                                 "S7": 0,
+                                 "S9": 0,
+                                 "D3": 0}
+    errors = False
+    miner_errors = {}
+
+    for miner in miners:
+        miner_stats = get_stats(miner.ip)
+        # if miner not accessible
+        if miner_stats['STATUS'][0]['STATUS'] == 'error':
+            errors = True
+            inactive_miners.append(miner)
+        else:
+            # Get worker name
+            miner_pools = get_pools(miner.ip)
+            worker = []
+            worker.append('[' + miner_pools['POOLS'][0]['User'] + '] ' + miner_pools['POOLS'][0]['URL'])
+            worker.append('[' + miner_pools['POOLS'][1]['User'] + '] ' + miner_pools['POOLS'][1]['URL'])
+            worker.append('[' + miner_pools['POOLS'][2]['User'] + '] ' + miner_pools['POOLS'][2]['URL'])
+
+
+            # Get miner's ASIC chips
+            asic_chains = [miner_stats['STATS'][1][chain] for chain in miner_stats['STATS'][1].keys() if
+                           "chain_acs" in chain]
+            # count number of working chips
+            O = [str(o).count('o') for o in asic_chains]
+            Os = sum(O)
+            # count number of defective chips
+            X = [str(x).count('x') for x in asic_chains]
+            Xs = sum(X)
+            # get number of in-active chips
+            _dash_chips = [str(x).count('-') for x in asic_chains]
+            _dash_chips = sum(_dash_chips)
+            # Get total number of chips according to miner's model
+            # convert miner.model.chips to int list and sum
+            chips_list = [int(y) for y in str(miner.model.chips).split(',')]
+            total_chips = sum(chips_list)
+            # Get the temperatures of the miner according to miner's model
+            temps = [int(miner_stats['STATS'][1][temp]) for temp in
+                     sorted(miner_stats['STATS'][1].keys(), key=lambda x: str(x)) if
+                     re.search(miner.model.temp_keys + '[0-9]', temp) if miner_stats['STATS'][1][temp] != 0]
+            # Get fan speeds
+            fan_speeds = [miner_stats['STATS'][1][fan] for fan in
+                          sorted(miner_stats['STATS'][1].keys(), key=lambda x: str(x)) if
+                          re.search("fan" + '[0-9]', fan) if miner_stats['STATS'][1][fan] != 0]
+            # Get GH/S 5s
+            ghs5s = miner_stats['STATS'][1]['GHS 5s']
+            # Get HW Errors
+            hw_error_rate = miner_stats['STATS'][1]['Device Hardware%']
+            # Get uptime
+            uptime = timedelta(seconds=miner_stats['STATS'][1]['Elapsed'])
+            #
+            workers.update({miner.ip: worker})
+            miner_chips.update({miner.ip: {'status': {'Os': Os, 'Xs': Xs, '-': _dash_chips},
+                                           'total': total_chips,
+                                           }
+                                })
+            temperatures.update({miner.ip: temps})
+            fans.update({miner.ip: {"speeds": fan_speeds}})
+            hash_rates.update({miner.ip: ghs5s})
+            hw_error_rates.update({miner.ip: hw_error_rate})
+            uptimes.update({miner.ip: uptime})
+            total_hash_rate_per_model[miner.model.model] += float(str(ghs5s))
+            active_miners.append(miner)
+
+            # Flash error messages
+            if Xs > 0:
+                error_message = "[WARNING] '{}' chips are defective on miner '{}'.".format(Xs, miner.ip)
+                logger.warning(error_message)
+                flash(error_message, "warning")
+                errors = True
+                miner_errors.update({miner.ip: error_message})
+            if Os + Xs < total_chips:
+                error_message = "[ERROR] ASIC chips are missing from miner '{}'. Your Antminer '{}' has '{}/{} chips'." \
+                    .format(miner.ip,
+                            miner.model.model,
+                            Os + Xs,
+                            total_chips)
+                logger.error(error_message)
+                flash(error_message, "error")
+                errors = True
+                miner_errors.update({miner.ip: error_message})
+            if max(temps) >= 80:
+                error_message = "[WARNING] High temperatures on miner '{}'.".format(miner.ip)
+                logger.warning(error_message)
+                flash(error_message, "warning")
+
+    # Flash success/info message
+    if not miners:
+        error_message = "[INFO] No miners added yet. Please add miners using the above form."
+        logger.info(error_message)
+        flash(error_message, "info")
+    elif not errors:
+        error_message = "[INFO] All miners are operating normal. No errors found."
+        logger.info(error_message)
+        flash(error_message, "info")
+
+    # flash("INFO !!! Check chips on your miner", "info")
+    # flash("SUCCESS !!! Miner added successfully", "success")
+    # flash("WARNING !!! Check temperatures on your miner", "warning")
+    # flash("ERROR !!!Check board(s) on your miner", "error")
+
+    end = time.clock()
+    loading_time = end - start
+    return render_template('myminers-dashboard.html',
+                           version=__version__,
+                           models=models,
+                           active_miners=active_miners,
+                           inactive_miners=inactive_miners,
+                           workers=workers,
+                           miner_chips=miner_chips,
+                           temperatures=temperatures,
+                           fans=fans,
+                           hash_rates=hash_rates,
+                           hw_error_rates=hw_error_rates,
+                           uptimes=uptimes,
+                           total_hash_rate_per_model=total_hash_rate_per_model,
+                           loading_time=loading_time,
+                           miner_errors=miner_errors,
+                           )
+
 
 @app.route('/add', methods=['POST'])
 def add_miner():
